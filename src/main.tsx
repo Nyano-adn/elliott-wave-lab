e91033531b9143b0700ae69bd9b605fb8f980fbe
Nyano-adn <yoann.audon@hotmail.fr>
2025-09-14 10:30:23 +0200

    chore: ensure full-screen mount and styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
+// src/main.tsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById('root')!;
+createRoot(el).render(<App />);
